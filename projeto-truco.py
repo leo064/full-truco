65c2fa75d4ac782f31a107e176287173929cc971
leo064 <stalinlayla@gmail.com>
2021-05-03 17:29:43 -0300

    added players' turn
    and your cards played

--- projeto-truco.py
+++ projeto-truco.py
@@ -31,37 +31,64 @@ def take_turncards():
     return turn_cards
 
 turn = take_turncards()
-print(turn)
-print("")
 
-"""'array list' (matriz) that gives the cards to respectives players"""
-player_deck = []
-first = 0
-last = 3
-for i in range(1, 5):
-    player = []
-    for j in range(first,last):
-        player.append(turn[j])
+def give_turncards():
+    """'array list' (matriz) that gives the cards to respectives players"""
+    p_decks = []
+    first = 0
+    last = 3
+    for i in range(1, 5):
+        player = []
+        for j in range(first,last):
+            player.append(turn[j])
 
-    player_deck.append(player)
+        p_decks.append(player)
 
-    first = last
-    last+=3
+        first = last
+        last+=3
 
-"""
+    return p_decks
+
+player_deck = give_turncards()
+
+"""define shakle of the turn:"""
+print("side is ...", turn[12]) # <--- side (last card) randomized in begin.
+side = turn_numcards[12]
+if side[0]+1 == 8: shackle = "Q"
+elif side[0]+1 == 9: shackle = "J"
+elif side[0]+1 == 10: shackle = "K"
+elif side[0]+1 == 11: shakle = "A"
+else: shackle = side[0]+1
+print("so... THE SHACKLE IS A: ", shackle)
+
 print(player_deck[0])
 print(player_deck[1])
 print(player_deck[2])
 print(player_deck[3])
-"""
-side = turn_numcards[12]
-print("side is ...", turn[12])
+print()
 
-if side[0]+1 == 8: shackle = "Q"
-elif side[0]+1 == 9: shackle = "J"
-elif side[0]+1 == 10: shackle = "K"
-else: shackle = side[0]+1
-print("so... THE SHACKLE IS A: ", shackle)
+"""define the player's turn"""
+for turn_of in range (4):
+    card_played = False
+    while (card_played == False):
+        print(player_deck[turn_of])
+        card1 = player_deck[turn_of][0]
+        card2 = player_deck[turn_of][1]
+        card3 = player_deck[turn_of][2]
+        card = input("What card you play? [1/2/3] :")
+        print("player {} played: ".format(turn_of+1))
+        if card == '1':
+            print(card1)
+        elif card == '2':
+            print(card2)
+        elif card == '3':
+            print(card3)
+        else:
+            print("invalid value!, try again!")
+            continue
+        print()
+        card_played = True
+
 
 
     
